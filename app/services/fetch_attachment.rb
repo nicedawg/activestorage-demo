fbc9c143003eb6923d103c838b77fab381ef4592
Brady Somerville <brady.somerville@gmail.com>
2019-01-07 19:46:10 -0600

    Refactor FetchAttachment service object

--- app/services/fetch_attachment.rb
+++ app/services/fetch_attachment.rb
@@ -1,27 +1,43 @@
+# This service takes an Attachment id and variant name
+# and returns an OpenStruct with the right data and content-type
 class FetchAttachment
   attr_accessor :id, :variant
 
   def initialize(id, variant)
-    self.id = id
-    self.variant = variant || "default"
+    @id = id
+    @variant = variant || "default"
   end
 
   def call
-    attachment = ActiveStorage::Attachment.find id.split("-")[0]
-
-    blob = attachment.blob
-
-    image_klass = (attachment.record_type + attachment.name.titleize + "::" + variant.titleize).constantize
-
-    blob_variant = if image_klass.variant_config
-      blob.representation(image_klass.variant_config).processed
-    else
-      blob
-    end
-
     OpenStruct.new(
       data: blob.service.download(blob_variant.key),
       content_type: blob.content_type || DEFAULT_SEND_FILE_TYPE
     )
+  end
+
+  private
+
+  def blob
+    @blob ||= attachment.blob
+  end
+
+  def attachment
+    @attachment ||= ActiveStorage::Attachment.find @id.split("-")[0]
+  end
+
+  def blob_variant
+    @blob_variant ||= if variant_config
+      blob.representation(variant_config).processed
+    else
+      blob
+    end
+  end
+
+  def variant_config
+    @variant_config ||= image_klass.variant_config
+  end
+
+  def image_klass
+    @image_klass ||= (attachment.record_type + attachment.name.titleize + "::" + @variant.titleize).constantize
   end
 end
